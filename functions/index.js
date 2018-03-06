f17a2af16bb7ffe958329e0cab77dce3641151df
petecarapetyan <pete@couldbe.net>
2018-03-06 11:38:46 -0600

    topics now write in dual, single, no web

--- functions/index.js
+++ functions/index.js
@@ -27,6 +27,10 @@ app.get('/webhook/', function(req, res){
   }
 });
 
+app.get('/dogleg/', function(req, res){
+    res.send("hi there");
+});
+
 app.post('/webhook/', function(req, res){
   let token = req.query['hub.verify_token'];
   let mode = req.query['hub.mode'];
